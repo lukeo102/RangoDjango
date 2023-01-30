962a84d579ae30fac36881fa85a1dd684df0769a
lukeo102 <lukeormiston@gmail.com>
2023-01-30 13:41:57 +0000

    Chapter 3 bug fixing

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>about</a>")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango'>home</a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
